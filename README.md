8d5965b5a41b5ca6e0466769f8732b46764c3eaf
22guerrabruno <22guerrabruno@gmail.com>
2023-09-29 17:33:04 +0200

    añado el juego completo

---
+++ README.md
@@ -0,0 +1,7 @@
+# Juego MeMoji´s - Memoria
+
+## Descripción
+
+Juego de memoria con temática de emojis, en el cual se debe encontrar la pareja de cada emoji, el juego cuenta con un cronometro y un contador de movimientos, al finalizar el juego se muestra un mensaje con el tiempo y los movimientos realizados.Cada acierto suma 20 puntos y los fallos restan 5 puntos.
+Tienes 5 segundos para memorizar los emojis en su sitio luego se esconderan y dispondras de 60 segundos para resolver el juego.
+Mucha Suerte y a Jugar!!!
---
+++ main.js
@@ -0,0 +1,130 @@
+let emojis = [
+  "😵‍💫",
+  "👻",
+  "❄️",
+  "👽",
+  "🎃",
+  "🦊",
+  "🐙",
+  "🌺",
+  "☠️",
+  "🧁",
+  "😵‍💫",
+  "👻",
+  "❄️",
+  "👽",
+  "🎃",
+  "🦊",
+  "🐙",
+  "🌺",
+  "☠️",
+  "🧁",
+];
+console.log(emojis);
+
+let showCards = 0;
+let time = 60;
+let cardOne = null;
+let cardTwo = null;
+let firstResult = null;
+let secondResult = null;
+let score = 0;
+let points = 0;
+let movements = 0;
+let temporizer = false;
+let regresiveTime = null;
+let tempoBack = document.getElementById("temp");
+let totalPoints = document.getElementById("points");
+let totalMovements = document.getElementById("movements");
+let totalScore = document.getElementById("score");
+
+function timer() {
+  regresiveTime = setInterval(() => {
+    time--;
+    tempoBack.innerHTML = `Temporizador ⌛️: ${time}`;
+
+    if (time === 0 && score < 10) {
+      clearInterval(regresiveTime);
+      blockTurnAllCards();
+      totalScore.innerHTML = `Total Aciertos: ${score}`;
+      totalPoints.innerHTML = `Total Puntos: ${points}`;
+      tempoBack.innerHTML = `Falto poco ...`;
+    }
+  }, 1000);
+}
+function blockTurnAllCards() {
+  for (let i = 0; i < emojis.length; i++) {
+    let card = document.getElementById(i);
+    card.innerHTML = emojis[i];
+  }
+}
+
+function start() {
+  emojis = emojis.sort(() => Math.random() - 0.5);
+  for (let i = 0; i < emojis.length; i++) {
+    let card = document.getElementById(i);
+    card.innerHTML = emojis[i];
+    card.disabled = true;
+  }
+
+  setTimeout(() => {
+    for (let i = 0; i < emojis.length; i++) {
+      let card = document.getElementById(i);
+      card.innerHTML = "";
+      card.disabled = false;
+    }
+  }, 5000);
+}
+
+function turn(id) {
+  if (temporizer == false) {
+    timer();
+    temporizer = true;
+  }
+
+  showCards++;
+
+  if (showCards === 1) {
+    cardOne = document.getElementById(id);
+    firstResult = emojis[id];
+    cardOne.innerHTML = firstResult;
+    cardOne.disabled = true;
+  } else if (showCards === 2) {
+    cardTwo = document.getElementById(id);
+    secondResult = emojis[id];
+    cardTwo.innerHTML = secondResult;
+    cardTwo.disabled = true;
+    movements++;
+
+    totalMovements.innerHTML = `Total Movimientos: ${movements}`;
+
+    if (firstResult === secondResult) {
+      showCards = 0;
+      score++;
+      points += 20;
+
+      totalScore.innerHTML = `Total Aciertos: ${score}`;
+      totalPoints.innerHTML = `Total Puntos: ${points}`;
+    } else {
+      points -= 5;
+      totalPoints.innerHTML = `Total Puntos: ${points}`;
+      setTimeout(() => {
+        cardOne.innerHTML = "";
+        cardTwo.innerHTML = "";
+        cardOne.disabled = false;
+        cardTwo.disabled = false;
+        showCards = 0;
+      }, 800);
+    }
+    if (score === 10) {
+      //confeti();
+      totalScore.innerHTML = `Total Aciertos: ${score}`;
+      totalPoints.innerHTML = `Total Puntos: ${points}`;
+      tempoBack.innerHTML = "Enhorabuena lo has logrado";
+    }
+  }
+}
+if (time === 0 && score < 10) {
+  clearInterval(regresiveTime);
+  blockTurnAllCards();
+}
